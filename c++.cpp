cec6c42f8e061efeb623c333f68ded458ea9e3cf
欧德心圣 <62007711+zhao-zongheng@users.noreply.github.com>
2020-06-09 11:47:48 +0800

    第一个GitHub文件
    用于学习使用GitHub网页。

---
+++ c++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int M[n][n];
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>M[i][j];
+        }
+    }
+    int sum[1][n];
+    for(int i=0;i<n;i++){
+        sum[0][i]=0;
+        for(int j=0;j<n;j++){
+            sum[0][i]+=M[j][i];
+        }
+        cout << sum[0][i] <<' ';
+    }
+
+    return 0;
+}
